24f1ed844fde33c03ed92fb506fc96e4402a063c
janssenjacob <jacobjanssen94@gmail.com>
2020-04-22 20:51:58 +0200

    Tabs and a list working

---
+++ app.js
@@ -0,0 +1,76 @@
+// Add element to list.
+function newElement() {
+  var li = document.createElement("ons-list-item");
+//   document.querySelector('ons-list-item').setAttribute('tappable', 'true');
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+	document.getElementById("myList").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+};
+
+/**
+* Pushes the listed pages.
+ */
+ function showPage(page) {
+	document.getElementById('myNavigator').pushPage(page);
+}
+
+/**
+* Everything which needs the DOM to be ready first.
+*/
+ons.ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("ons-list-item");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+  
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ons-list');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'ONS-LIST-ITEM') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+});
+});
+
+
+
+
